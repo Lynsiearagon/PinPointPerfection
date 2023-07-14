9ccea7e0de507869b2917b79741e6db56ad537be
Lynsie Aragon <lynsie.aragon@gmail.com>
2023-07-14 18:30:10 -0400

    Build arsenal class

--- src/scripts/arsenal.js
+++ src/scripts/arsenal.js
@@ -1,16 +1,36 @@
-import ball from src/scripts/ball.js
-
 class Arsenal {
   
+  // arsenal is an array of bowling ball names
   constructor() {
     this.arsenal = []
   };
   
+
+  // Show arsenal
   showArsenal() {
     return this.arsenal
   };
 
 
+  // Delete bowling ball from arsenal
+  removeBallFromArsenal(bowlingBall) {
+    
+    for (let i = 0; i < this.arsenal.length; i++) {
+      if (this.arsenal[i] === bowlingBall.name) {
+        this.arsenal.delete(arsenal[i])
+      }
+    }
+
+    return this.arsenal;
+  };
+
+
+  // Add bowling ball to arsenal
+  addBallToArsenal(bowlingBall) {
+    this.arsenal.push(bowlingBall);
+  };
+
+
 }
 
-module.exports = Arsenal
+export default Arsenal; 
